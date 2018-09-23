e585256e835ec187f0fb7f140b14ee3ac5e7d1da
eviless <sekerin.e.a@gmail.com>
2018-09-23 22:21:54 +0500

    * backend * routes + files

--- backend/tests/Controller/FileControllerTest.php
+++ backend/tests/Controller/FileControllerTest.php
@@ -11,7 +11,7 @@ class FileControllerTest extends WebTestCase
     public function testIndex()
     {
         $client = static::createClient();
-        $client->request('GET', '/');
+        $client->request('GET', '/files/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode(),
             'GET / must be accessible');
@@ -23,7 +23,7 @@ class FileControllerTest extends WebTestCase
     public function testDownload()
     {
         $client = static::createClient();
-        $client->request('GET', '/filename');
+        $client->request('GET', '/files/filename');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode(),
             'GET /filename must be accessible');
@@ -32,12 +32,12 @@ class FileControllerTest extends WebTestCase
     public function testSave()
     {
         $client = static::createClient();
-        $client->request('POST', '/');
+        $client->request('POST', '/files/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode(),
             'POST / must be accessible');
 
-        $client->request('POST', '/filename');
+        $client->request('POST', '/files/filename');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode(),
             'POST /filename must be accessible');
@@ -46,7 +46,7 @@ class FileControllerTest extends WebTestCase
     public function testDelete()
     {
         $client = static::createClient();
-        $client->request('DELETE', '/filename');
+        $client->request('DELETE', '/files/filename');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode(),
             'DELETE /filename must be accessible');

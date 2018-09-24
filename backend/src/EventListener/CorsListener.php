f16a499b006ff604cffc648aa1ad49b101acb596
eviless <sekerin.e.a@gmail.com>
2018-09-24 07:10:21 +0500

    * backend * CorsListener + cors allow-origin from config

--- backend/src/EventListener/CorsListener.php
+++ backend/src/EventListener/CorsListener.php
@@ -5,15 +5,27 @@ declare(strict_types=1);
 namespace App\EventListener;
 
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class CorsListener
 {
+    protected $corsHosts;
+
+    /**
+     * CorsListener constructor.
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->corsHosts = $container->getParameter('cors_accept') ?? '*';
+    }
+
     public function onKernelResponse(FilterResponseEvent $event): void
     {
         $responseHeaders = $event->getResponse()->headers;
 
         $responseHeaders->set('Access-Control-Allow-Headers', 'content-type, accept');
-        $responseHeaders->set('Access-Control-Allow-Origin', 'http://sf.local');
+        $responseHeaders->set('Access-Control-Allow-Origin', $this->corsHosts);
         $responseHeaders->set('Access-Control-Allow-Credentials', 'true');
     }
 }
